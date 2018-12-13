268f0636304226b44f9f5367da7351b01b87978e
Yaroslav Pakhalyuk <yaroslav.pakhalyuk@hhstechgroup.com>
2018-12-13 16:31:37 +0200

    hi leha

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
 const canvas = document.getElementById('game-canvas');
 const canvasContext = canvas.getContext('2d');
 
+const generateBrickSMap = (count) => {
+  const maxRowSize = Math.trunc(canvas.clientWidth / BRICK_WIDTH);
+
+  const rowCount = Math.ceil(count / maxRowSize);
+
+  const map = [];
+
+  for (let i = 0; i < count; i++) {
+    if (i === 0 || i % maxRowSize === 0) {
+      map.push([]);
+    }
+
+    map[map.length - 1].push({ isLive: true });
+  }
+  return map;
+}
+
 canvas.setAttribute('width', window.innerWidth);
 canvas.setAttribute('height', window.innerHeight);
 
+const circleStaticSpeed = 10;
 const circleRadiurs = 20;
 
-let circleX = circleRadiurs;
-let circleY = circleRadiurs;
-
-let circleSpeedX = 10;
-let circleSpeedY = 10;
-
-const PADDLE_WIDTH = 100;
+const PADDLE_WIDTH = 200;
 const PADDLE_HEIGHT = 10;
+
+const BRICK_WIDTH = 100;
+const BRICK_HEIGHT = 20;
+const BRICK_SPACE = 10;
+const bricksMap = generateBrickSMap(150);
+
+let circleX = canvas.clientWidth / 2;
+let circleY = canvas.clientHeight / 2;
+
+let circleSpeedX = circleStaticSpeed;
+let circleSpeedY = circleStaticSpeed;
+
 
 let paddleX = canvas.clientWidth / 2 - PADDLE_WIDTH;
 let paddleY = canvas.clientHeight - PADDLE_HEIGHT;
@@ -22,10 +46,13 @@ let paddleY = canvas.clientHeight - PADDLE_HEIGHT;
 
 canvas.addEventListener('mousemove', e => {
   paddleX = e.clientX - PADDLE_WIDTH / 2;
+
+  // For debug
+  // circleX = e.clientX;
+  // circleY = e.clientY;
 });
 
 const gameLoop = () => {
-
   circleX += circleSpeedX;
   circleY += circleSpeedY;
 
@@ -47,18 +74,48 @@ const gameLoop = () => {
   if (
     circleX > paddleX &&
     circleX < paddleX + PADDLE_WIDTH &&
-    circleY > paddleY &&
-    circleY < paddleY + PADDLE_HEIGHT
+    circleY > paddleY
   ) {
     circleSpeedY *= -1;
+
+    const centerOfPaddleX = paddleX + PADDLE_WIDTH / 2;
+    const ballDistanceFromPaddleCenterX = circleX - centerOfPaddleX;
+
+    circleSpeedX = ballDistanceFromPaddleCenterX * (circleStaticSpeed / PADDLE_WIDTH);
   }
 
   drawReact(0, 0, canvas.clientWidth, canvas.clientHeight, '#37474F');
 
+  drawBricks();
   drawReact(paddleX, paddleY, PADDLE_WIDTH, PADDLE_HEIGHT, '#F5F5F5'); // paddle
   drawCircle(circleX); // ball
 
   requestAnimationFrame(gameLoop);
+}
+
+const drawBricks = () => {
+  bricksMap.forEach((row, i) => {
+    row.forEach((brick, j) => {
+      const BRICK_X = BRICK_WIDTH * j + BRICK_SPACE * j;
+      const BRICK_Y = BRICK_HEIGHT * i + BRICK_SPACE * i;
+
+      if (
+        brick.isLive &&
+        circleX + circleRadiurs > BRICK_X &&
+        circleX - circleRadiurs < BRICK_X + BRICK_WIDTH &&
+        circleY + circleRadiurs > BRICK_Y &&
+        circleY - circleRadiurs < BRICK_Y + BRICK_HEIGHT
+      ) {
+        brick.isLive = false;
+
+        if (circleSpeedY < 0) {
+          circleSpeedY *= -1;
+        }
+      }
+
+      if (brick.isLive) drawReact(BRICK_X, BRICK_Y, BRICK_WIDTH, BRICK_HEIGHT, '#FBE9E7');
+    });
+  });
 }
 
 const drawReact = (x, y, width, height, color) => {
